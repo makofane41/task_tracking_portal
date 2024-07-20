e9ab9db6b32da2bd0a20d0fa5949c32ee608fcc8
Given Makofane <93738259+makofane41@users.noreply.github.com>
2024-07-20 13:07:57 +0200

    set all views
    ec2 test

--- taskapp/views.py
+++ taskapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'taskapp/task.html',{})
+
+def register(request):
+    return render(request,'taskapp/register.html',{})
+
+def login(request):
+    return render(request,'taskapp/login.html',{})
